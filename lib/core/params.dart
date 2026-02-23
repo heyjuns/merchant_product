53e9d28e7ca8da9ad315d07b7caaa4fe5e424cf4
Juanda <heyjuns@icloud.com>
2026-02-23 15:01:40 +0700

    remove unused cancelToken

--- lib/core/params.dart
+++ lib/core/params.dart
@@ -1,10 +1,7 @@
-import 'package:dio/dio.dart';
-
 class Params {
   final String? endPoint;
   final Map<String, dynamic>? queryParameters;
   final Map<String, dynamic>? body;
-  final CancelToken? cancelToken;
 
-  Params({this.endPoint, this.queryParameters, this.body, this.cancelToken});
+  Params({this.endPoint, this.queryParameters, this.body});
 }
